7e38e251f27b528332ef14ef6d9bad70f3c1314f
chewchooo <324687913+chewchooo@users.noreply.github.com>
2026-09-16 09:10:19 -0400

    CFB Edge - 2026 wk3 - 2026-09-19 11:32

--- cfb-edge/meta.js
+++ cfb-edge/meta.js
@@ -1,1 +1,1 @@
-window.CFB_META={"generated":"2026-09-16T06:50:41","season":2026,"week":3,"mode":"upcoming","counts":{"games":75,"teams":139}};
+window.CFB_META={"generated":"2026-09-19T11:32:32","season":2026,"week":3,"mode":"upcoming","counts":{"games":71,"teams":139}};
